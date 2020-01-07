97b6a9df052d416d7ba7631f493be6c927c88591
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2020-01-07 11:30:56 -0500

    function_testWord without notes

---
+++ function_testWord_without_notes.js
@@ -0,0 +1,8 @@
+function testWord(word){
+    if(word === "homosexuals" || words === "Homosexuals"){
+        return true;
+    } else {
+        return false
+    }
+}
+console.log(testWord("love"));
