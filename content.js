1d52976e1fdc537df7184b9b0f138e2c401e1ac2
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2020-01-15 13:04:45 -0500

    Bug fixed for content.js file

--- content.js
+++ content.js
@@ -1,21 +1,18 @@
 var elements = document.getElementsByTagName('*');
-
 for (var i = 0; i < elements.length; i++){
-
     var element = elements[i];
-
         for(var j = 0; j < element.childNodes.length; j++){
-            var node = element.childNodes[j];
-          
-        }
-            if(node.nodeType === 3 ) {
+            var node = element.childNodes[j];      
+        
+            if(node.nodeType === 3 || node.nodeType === 2) {
                 var text = node.nodeValue;
                 var replacedText = text.replace(/homosexuals/gi, 'individuals who self-identify as LGBTQ+');
-
-                if(replacedText !== text) {
-                    element.replaceChild(document.createTextNode(replacedText), node);
-                }
-        }
+            
+            if(replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
      
-         }
-    
+        }
+        
+    }
+}
